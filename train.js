fb2589e15eadfd3e30a78b340002c92b0ba28207
dkumar13 <34710266+dkumar13@users.noreply.github.com>
2018-04-13 09:58:01 +0530

    livetrainstatus

--- train.js
+++ train.js
@@ -1,24 +1,24 @@
 ///<reference path="./angular.js" />
 
-let pnr,pnrVal,xhr,parjsn;
+let pnr, pnrVal, xhr, parjsn;
 let imgdata;
 let getclick
 
 let myApp = angular.module("myModule", ["ngRoute"]);
 
-myApp.controller("myController", function($scope, $http){
+myApp.controller("myController", function ($scope, $http) {
 
-    $scope.getpnr = function(){
-        
+    $scope.getpnr = function () {
+
         pnr = document.getElementById("getval");
         //alert("PNR Value = " + pnr.value)
-        if(pnr!="undefined"  || pnr !="null"){
+        if (pnr != "undefined" || pnr != "null") {
 
             $http({
-                method:"GET",
-                url:"https://api.railwayapi.com/v2/pnr-status/pnr/"+pnr.value+"/apikey/2afh9rxhwo/"
+                method: "GET",
+                url: "https://api.railwayapi.com/v2/pnr-status/pnr/" + pnr.value + "/apikey/2afh9rxhwo/"
             }).then(
-                function(response){
+                function (response) {
                     $scope.data = response.data;
                     console.log($scope.data)
                     console.log($scope.data.passengers[0])
@@ -29,67 +29,118 @@ myApp.controller("myController", function($scope, $http){
 
                     //$digest
                 },
-            function(response){
-                $scope.status = response.status;
-                console.log($scope.status)
-            });
+                function (response) {
+                    $scope.status = response.status;
+                    console.log($scope.status)
+                });
         }
+    }
+    $scope.trnno = function () {
+
+        var pnr = document.getElementById("gettn");
+        var dte = document.getElementById("getdat");
+        //alert("PNR Value = " + pnr.value)
+        if (pnr != "undefined" || pnr != "null") {
+            
+            $http({
+                method: "GET",
+                url: "https://api.railwayapi.com/v2/live/train/" + pnr.value + "/date/" + dte.value + "/apikey/2afh9rxhwo/"
+            }).then(
+                function (response) {
+                    $scope.data = response.data;
+                    console.log($scope.data)
+                    //console.log($scope.data.passengers[0])
+                    //alert(pnr)
+                    // document.getElementById("displcss").style.display = "block";
+                    //$digest
+                },
+                function (response) {
+                    $scope.status = response.status;
+                    console.log($scope.status)
+                });
+                seatavcss();
+        }
+    }
+
+    $scope.checkcss = function (){
+
+        document.getElementById("pagdat").style.marginTop = "125px";
+
     }
 
 })
 
 
 
-myApp.controller("allImages", function($scope){
+
+
+
+
+
+
+
+
+myApp.controller("allImages", function ($scope) {
 
     imgdata = {
-        logo:"./trn_bck2.jpg"
+        logo: "./trn_bck2.jpg"
     }
 
     $scope.imgdata = imgdata;
 })
 
 
-myApp.config(function($routeProvider){
+myApp.config(function ($routeProvider) {
 
-    $routeProvider.when("/",{
-        templateUrl:"main.html",
-        controller:"addevent"
+    $routeProvider.when("/", {
+        templateUrl: "main.html",
+        controller: "addevent"
     })
-    .when("/pnr",{
-        templateUrl:"pnr.html",
-        controller:"myController"
-        
-    })
-    .otherwise({
-        templateUrl:"main.html"
-    })
-    
+        .when("/pnr", {
+            templateUrl: "pnr.html",
+            controller: "myController"
+
+        })
+        .when("/seat", {
+            templateUrl: "seat.html",
+            controller: "myController"
+
+        })
+        .otherwise({
+            templateUrl: "main.html"
+        })
+
 })
 
-myApp.controller("addevent", function($scope,$timeout){
+myApp.controller("addevent", function ($scope, $timeout) {
 
-    $timeout(()=>{
-     getclick = document.getElementById("dismen")
-         
-    },1000)
+    $timeout(() => {
+        getclick = document.getElementById("dismen")
 
-    $scope.hidedata = ()=>{
-        document.getElementById("trndl1").style.display="flex";
-        getclick.style.display="none"
-		document.getElementById("pagdat").style.marginTop="0px";
+    }, 1000)
+
+    $scope.hidedata = () => {
+        document.getElementById("trndl1").style.display = "flex";
+        getclick.style.display = "none"
+        document.getElementById("pagdat").style.marginTop = "0px";
 
     }
 
 })
 
 
-const allstyle = ()=>{
-    
-    document.getElementById("pagdat").style.marginTop="0px";
-	document.getElementById("getval").style.marginLeft="85px";
-    document.getElementById("pnrchart").style.display="block";
-    document.getElementById("jrnif").style.display="block";
-    document.getElementById("displcss").style.display="block";
+const allstyle = () => {
+
+    document.getElementById("pagdat").style.marginTop = "0px";
+    document.getElementById("getval").style.marginLeft = "95px";
+    document.getElementById("pnrchart").style.display = "block";
+    document.getElementById("jrnif").style.display = "block";
+    document.getElementById("displcss").style.display = "block";
 }
 
+const seatavcss = () =>{
+
+    document.getElementById("data").style.display = "block";
+    document.getElementById("pagdat").style.marginTop = "0px";
+    document.getElementById("gettn").style.marginLeft = "221px";
+}
