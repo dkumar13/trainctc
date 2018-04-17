fb7caa43a355ca48882e026e7e7a426f35a3bd97
dkumar13 <34710266+dkumar13@users.noreply.github.com>
2018-04-18 01:10:07 +0530

    deploy

--- train.js
+++ train.js
@@ -106,6 +106,11 @@ myApp.config(function ($routeProvider) {
             controller: "myController"
 
         })
+        .when("/traininfo", {
+            templateUrl: "traininfo.html",
+            controller: "myController"
+
+        })
         .otherwise({
             templateUrl: "main.html"
         })
@@ -127,7 +132,73 @@ myApp.controller("addevent", function ($scope, $timeout) {
     }
 
 })
+myApp.filter("dayss", function(){
+    return function(input){
+        switch(input){
+            case "Y":
+            return "Running";
+            break;
 
+            case "N":
+            return "Not Running";
+            break;
+            
+            default:
+            return "Wrong Data";
+        }
+    }
+})
+
+    myApp.filter("classess", function(){
+    return function(input){
+        switch(input){
+            case "Y":
+            return "Available";
+            break;
+
+            case "N":
+            return "Not Available";
+            break;
+            
+            default:
+            return "Wrong Data";
+        }
+    }
+})
+    myApp.controller("myController",function($scope, $http){
+
+
+
+        $scope.getdetl  = function(){
+    var trnsd = document.getElementById("gettnnm");
+    //var dte = document.getElementById("getdat");
+    //alert("PNR Value = " + pnr.value)
+    if (trnsd != "undefined" || trnsd != "null") {
+        
+        $http({
+            method: "GET",
+            url: "https://api.railwayapi.com/v2/name-number/train/"+trnsd.value+"/apikey/2afh9rxhwo/"
+        }).then(
+            function (response) {
+                $scope.data = response.data;
+                console.log($scope.data)
+                console.log($scope.data.train.name)
+                //console.log($scope.data.passengers[0])
+                //alert(pnr)
+                // document.getElementById("displcss").style.display = "block";
+                //$digest
+                cssprop();
+            },
+            function (response) {
+                $scope.status = response.status;
+                console.log($scope.status)
+            });
+            
+    }
+        }
+
+
+    })
 
 const allstyle = () => {
 
@@ -143,4 +214,13 @@ const seatavcss = () =>{
     document.getElementById("data").style.display = "block";
     document.getElementById("pagdat").style.marginTop = "0px";
     document.getElementById("gettn").style.marginLeft = "221px";
+}
+
+const cssprop = () =>{
+
+    document.getElementById("maindiv").style.display = "flex"; 
+    document.getElementById("pagdat").style.marginTop = "0px";
+    document.getElementById("gettnnm").style.marginLeft = "148px";
+    document.getElementById("gettnnm").style.marginBottom = "15px";
+      
 }
